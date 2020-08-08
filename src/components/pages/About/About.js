1151c569d486ed61c6cc147573d9f9aa4a45e7b2
Tim Winters <61638296+erasersleeve@users.noreply.github.com>
2020-08-08 15:27:47 -0400

    heroku errors

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -7,16 +7,16 @@ function About() {
     return(
     <>
         <Navbar></Navbar>
-        <main class="container">
+        <main className="container">
             
-            <section class="row mt-5 p-3 border-bottom border-dark bg-light rounded">   
+            <section className="row mt-5 p-3 border-bottom border-dark bg-light rounded">   
               <h1>
                 About Me
               </h1>
             </section>
                   
-            <section class="row pt-3 pb-3 rounded">
-                <section class="col-7">   
+            <section className="row pt-3 pb-3 rounded">
+                <section className="col-7">   
                     <img src={Picture} alt="headshot" className="headshot pr-3"/>
                     <p>
                         Hello and welcome! My name is Tim Winters and this is my portfolio site. I'm a 26 year old with a background in education and the humanities trying to start a new career path. On my portfolio page you can find some examples of my work, and if you want to know more or reach out to me you can find links to my other accounts as well as a form to send me a message. 
--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -19,7 +19,7 @@ function Contact() {
     return(
     <>
     <Navbar></Navbar>
-       <section class="row mt-5 p-3 border-bottom border-dark bg-light rounded">
+       <section className="row mt-5 p-3 border-bottom border-dark bg-light rounded">
             
             <h1>
                 Contact
@@ -27,18 +27,18 @@ function Contact() {
                             
         </section>
                 
-        <section class="row rounded pb-3">
+        <section className="row rounded pb-3">
             
-            <div class="col-12 col-md-8"> 
+            <div className="col-12 col-md-8"> 
                 <form action="action_page.php" onSubmit={handleSubmit}>
             
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input type="text" id="name" name="name" placeholder="Your name.." onChange={e => setName(e.target.value)}/>
                     
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="text" name="email" id="email" placeholder="Email.." onChange={e => setEmail(e.target.value)}/>
 
-                    <label for="subject">Subject</label>
+                    <label htmlFor="subject">Subject</label>
                     {/* style={"height:200px"} */}
                     <textarea id="subject" name="subject" placeholder="Write something.."onChange={e => setSubject(e.target.value)}/>
                 
@@ -48,14 +48,14 @@ function Contact() {
             </div>     
 
             {/* style="width: 18rem;" */}
-            <aside class="card col-12 col-md-4 border-bottom-0">    
-                <div class="card-body text-center">
-                    <h2 class="card-title border-bottom">External Links</h2>
-                    <a href="https://github.com/erasersleeve" class="link">Github</a>
+            <aside className="card col-12 col-md-4 border-bottom-0">    
+                <div className="card-body text-center">
+                    <h2 className="card-title border-bottom">External Links</h2>
+                    <a href="https://github.com/erasersleeve" className="link">Github</a>
                     <br></br>
-                    <a href="https://www.linkedin.com/in/timwinters99/" class="link">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/timwinters99/" className="link">LinkedIn</a>
                     <br></br>
-                    <a href="https://github.com/erasersleeve/Responsive-Portfolio/blob/master/assets/supplemental/Resume.pdf" class="link">Resume</a>
+                    <a href="https://github.com/erasersleeve/Responsive-Portfolio/blob/master/assets/supplemental/Resume.pdf" className="link">Resume</a>
                 </div>
                    
             </aside>           
